7d3db6e734732725a54cd072644e1ccff5031a41
antizhy <antizhy@outlook.com>
2022-02-21 14:31:10 +0000

    fix bugs

--- ts/main.ts
+++ ts/main.ts
@@ -2,20 +2,56 @@ const MIMGB = './assets/zhy2.png';
 const MIMGA = './assets/zhy1.png';
 
 class ScoreCount {
-    public constructor(id: string) {
+    public constructor(id: string, tipsId: string) {
         this.element = document.getElementById(id) as HTMLSpanElement;
+        this.tipsElement = document.getElementById(tipsId) as HTMLSpanElement;
     }
     private _score = 0;
     public set score(val: number) {
         this._score = val;
-        this.element!.innerText = `得分: ${this.score}`;
+        this.element!.innerText = `得分: ${this.score} miss: ${this.miss}`;
     }
 
     public get score() {
         return this._score;
     }
+
+    public get miss() {
+        return this._miss;
+    }
+
+    public set miss(val: number) {
+        this._miss = val;
+        this.element!.innerText = `得分: ${this.score} miss: ${this.miss}`;
+    }
     private element?: HTMLSpanElement;
-    public continuous = false;
+    private tipsElement?: HTMLSpanElement;
+    private hitContinCount = 0;
+    private _miss = 0;
+
+    public set continuous(val: boolean) {
+        this._continuous = val;
+        if (val) {
+            this.hitContinCount++;
+            this.tipsElement!.innerText = `连击${this.hitContinCount}次`;
+        }
+        else {
+            if (this.hitContinCount > 0) {
+                this.miss++;
+                this.tipsElement!.innerText = `连击失败`;
+                this.hitContinCount = 0;
+            }
+            else {
+                this.tipsElement!.innerText = '连击可获得更多得分';
+            }
+        }
+    }
+
+    public get continuous() {
+        return this._continuous;
+    }
+
+    private _continuous = false;
 
     public addScore(scores: number) {
         if (this.continuous) {
@@ -24,6 +60,13 @@ class ScoreCount {
         else {
             this.score += scores;
         }
+    }
+
+    public clear() {
+        this._miss = 0;
+        this._score = 0;
+        this.hitContinCount = 0;
+        this._continuous = false;
     }
 
 }
@@ -55,7 +98,6 @@ class HoleUnit {
     private element?: HTMLDivElement;
     private handle = 0;
     private isUp = false;
-    private hitCount = 0;
     private scoreCount?: ScoreCount;
     private audioPlayer?: HTMLAudioElement;
 
@@ -71,7 +113,8 @@ class HoleUnit {
         this.setScoreCount(scoreCount);
         this.element.onclick = () => {
             if (this.isUp) {
-                this.hitCount++;
+                this.isUp = false;
+                this.audioPlayer!.currentTime = 0;
                 this.audioPlayer!.play();
                 this.imgBox!.src = MIMGA;
                 this.scoreCount!.addScore(1);
@@ -80,7 +123,7 @@ class HoleUnit {
                 setTimeout(() => {
                     this.decline();
                 }, 300);
-                
+
                 // this.imgBox!.src = MIMGB;
                 // this.start();
             }
@@ -88,7 +131,7 @@ class HoleUnit {
                 //this.imgBox!.src = MIMGA;
                 //this.hitCount++;
                 this.scoreCount!.continuous = false;
-                
+
             }
         };
     }
@@ -114,6 +157,7 @@ class HoleUnit {
             this.rise();
             // this.isUp = true;
             this.handle = setTimeout(() => {
+                this.scoreCount!.continuous = false;
                 this.decline();
             }, this.waitDuration);
         }, 0);
@@ -135,8 +179,11 @@ window.onload = () => {
     const units: HoleUnit[] = [];
     const player = document.getElementById('sfx') as HTMLAudioElement;
     const bgmPlayer = document.getElementById('bg') as HTMLAudioElement;
-    const scoreCount = new ScoreCount('score');
+    const scoreCount = new ScoreCount('score', 'tips');
     const stBtn = document.getElementById('btn') as HTMLButtonElement;
+    const limited = 20;
+    let times = 0;
+    let last = -1;
     stBtn.onclick = () => {
         stBtn.style.display = 'none';
         bgmPlayer.play();
@@ -146,10 +193,27 @@ window.onload = () => {
         units.forEach(e => e.init(scoreCount));
         let random = new Random();
         let handle = setInterval(() => {
+            if (times >= limited) {
+                clearInterval(handle);
+                setTimeout(() => {
+                    alert(`得分: ${scoreCount.score} miss: ${scoreCount.miss}`);
+                    stBtn.style.display = 'block';
+                    times = 0;
+                    scoreCount.clear();
+                }, 2500);
+
+            }
+            times++;
             let next = random.Next(0, 5);
-            units[next].start();
-        }, 1500);
+            if (next == last) {
+                setTimeout(() => units[next].start(), 800);
+            }
+            else {
+                units[next].start();
+            }
+            last = next;
+        }, 1000);
     };
-    
+
     //units[0].start();
 }; 
